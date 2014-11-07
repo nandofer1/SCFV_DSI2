8ced47e270baa147c07a7a92d4526f228e6dd61a
juan-gamez <asalvodelolvido@gmail.com>
2014-11-07 12:53:07 -0600

    Update Management.php

--- app/Model/Management.php
+++ app/Model/Management.php
@@ -1,19 +1,17 @@
 <?php
 class Management extends AppModel
 {
+    public $belongsTo = array('Unit' => array('foreignKey'=>'unit_id')); //belongsTo: the current model contains the foreign key.
      public $validate=array( //INICIO VALIDACION
           
-         'unit_id'=>array(
+        'unit_id'=>array(
             'rule'=>'notEmpty',
             'message' => 'Debe Seleccionar una departamento'),
          
-         'gerencia' => array(
+        'gerencia' => array(
         'valid' => array('rule' => array('custom','^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$^'), 
                        'message' => 'Ingrese un nombre de gerencia válido'),
         'required' => array('rule' => array('minLength', '6'),
                     'message' => 'Ingrese un Nombre de gerencia válido')),
-         
-         
-         
          );//FIN VALIDACION
 }
