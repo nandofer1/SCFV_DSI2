49dabce7e03732e5b537479050ac07126d473e8b
juan-gamez <asalvodelolvido@gmail.com>
2014-12-01 22:38:19 -0600

    Update UserType.php

--- app/Model/UserType.php
+++ app/Model/UserType.php
@@ -6,13 +6,18 @@ class UserType extends AppModel {
     public $validate = array(
         'tipo_usuario' => array(
             'required' => array(
-                'rule' => array('notEmpty'),
-                'message' => 'Este campo no debe quedar vacio.'
+              'rule' => 'notEmpty',
+              'message' => 'Este campo no debe quedar vacio.'
             ), 
             'unique' => array(
               'rule' => 'isUnique',
-              'required' => 'create',
+              'required' => true,
               'message' => 'Este campo debe ser unico.'
+            ),
+            'alphaNumeric' => array(
+              'rule' => array('custom','^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$^'), 
+              'required' => true,
+              'message' => 'Este campo solo debe contener letras.'
             )
         )
     );
