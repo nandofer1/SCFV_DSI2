d4346721058b00282e1b4ed486e9ed7f75e15566
juan-gamez <asalvodelolvido@gmail.com>
2014-12-11 23:36:55 -0600

    Update Tool.php

--- app/Model/Tool.php
+++ app/Model/Tool.php
@@ -29,26 +29,28 @@ class Tool extends AppModel
               'rule' => 'isUnique',
               'required' => 'create',
               'message' => 'La Herramienta ya esta registrada.'
-            ))
-         /*
+            )),
+
           'existencia' => array(
+      'between' => array('rule' => array('between', 1, 99999),
+                            'message' => 'Debe ser un numero positivo'),
       'valid' => array('rule' => array('custom','/^[0-9]{1,}$/i'),
-                       'message' => ' Existencia solo debe contener numeros enteros'),
-      'required' => array('rule' => array('maxLength', '4'),
-                            'message' => 'Existencia Máximo 4 caracteres')),
-         
+                       'message' => ' Existencia solo debe contener numeros enteros')),
+
+
+         /*
          'descripcion' => array(
         'valid' => array('rule' => array('custom','/^[A-Za-z0-9áéíóúÁÉÍÓÚÑñ\s]{0,}$/i'), // ->> solo letras y numeros minímo 10 caracteres MAYUSCULA Y MINUSCULA
                        'message' => 'Ingrese una descripción válida'),
         'required' => array('rule' => array('minLength', '1'),
                             'message' => 'Ingrese una descripción válida')),
-         
+         */
          'valor' => array(
-      'valid' => array('rule' => array('decimal','2'),
+      'valid' => array('rule' => array('money'),
                        'message' => 'Ingrese una cantidad monetaria válida Ej:2.00'),
       'required' => array('rule' => array('minLength', '1'),
                             'message' => 'Debe Escribir una cantidad'))
         
-        */
+        
     ); //FIN VALIDACION
 }
