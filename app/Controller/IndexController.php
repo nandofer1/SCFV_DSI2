02fc43b9a9cdef2680afe8a9f469eab1a5ec7c61
juan-gamez <asalvodelolvido@gmail.com>
2014-12-13 00:21:39 -0600

    Create IndexController.php

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+/*
+App::uses('AppController', 'Controller');
+App::uses('UserType', 'Model');
+
+class IndexController extends AppController {
+
+  public function isAuthorized($user) {
+	  if (in_array($this->action, array('index'))) {
+  		return true;
+		}
+	}
+ 
+  public function index() {
+    return $this->redirect("/");
+  }
+}
+*/
